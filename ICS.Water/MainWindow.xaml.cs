7896467441322d7464c8ae19fa2308751482a820
rockiez <simpi@foxmail.com>
2015-11-28 23:39:45 +0800

    created.

---
+++ ICS.Water/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ICS.Common;
+using ICS.Acquisition;
+namespace ICS.Water
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        ResultEntity state = Global.KL_M4514Provider.CheckSerialPort(Global.KL_M4514Provider);
+
+        bool heatstate;
+
+        private void controlHeatingButtor (object sender, MouseButtonEventArgs e)
+        {
+            if (state.Status == RunStatus.Success)
+            {
+                var commond = Global.ADAM4150Provider;
+                    if (commond.OnOff(heatstate ? ADAM4150FuncID.OffSocket1 : ADAM4150FuncID.OnSocket1))
+                    {
+                        textOnOff.Text = heatstate ? "STOP" : "START" ;
+                        heatstate = !heatstate;
+                    }
+                    else
+                    {
+                        MessageBox.Show("操作失败");
+                    }
+            }
+        }
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            var timer = new LazyTimer(_sender =>
+            {
+                var t = (LazyTimer) _sender[0];
+
+                if (state.Status == RunStatus.Success)
+                {
+                    var Value = Global.KL_M4514Provider;
+                    Value.SetData();
+                    Application.Current.Dispatcher.Invoke(() =>
+                    {
+                        WaterLevel.Text = Value.waterLevelValue;
+                        WaterTemperature.Text = Value.waterTemperatureValue;
+                    });
+                }
+
+                t.Reset();
+            }, 100, 1000);
+        }
+
+
+        private void Close_OnClick(object sender, RoutedEventArgs e)
+        {
+        Close();
+        }
+
+        private void Minstate_OnClick(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+    }
+}
---
+++ ICS.Water/Resources/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ICS.Acquisition;
+using ICS.Models;
+using ICS.Common;
+
+namespace ICS.Water
+{
+    class Global
+    {
+        public static KL_M4514 KL_M4514Provider
+        {
+            get { return (KL_M4514) ClassFactory.GetProvider(equipmentCategory.KL_M4514, ComSetting); }
+        }
+
+        public static ADAM4150 ADAM4150Provider
+        {
+            get { return (ADAM4150) ClassFactory.GetProvider(equipmentCategory.ADAM4150, ComSetting); }
+        }
+
+        public static Models.Com.ComSettingModel _ComSetting;
+
+        public static Models.Com.ComSettingModel ComSetting
+        {
+            get
+            {
+                if (_ComSetting == null)
+                {
+                    _ComSetting = new SettingHelper<Models.Com.ComSettingModel>().GetSetting();
+                    _ComSetting.DigitalQuantityCom = "Com3";
+                    _ComSetting.AnalogQuantityCom = "Com3";
+                }
+                return _ComSetting;
+            }
+        }
+    }
+}
